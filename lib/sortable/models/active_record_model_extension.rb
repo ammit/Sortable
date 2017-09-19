4fa25ba319bcfe1b944ff71084f110e0f27d828e
Amit K <amit@lainfotech.in>
2017-09-20 02:59:37 +0530

    default sortings

--- lib/sortable/models/active_record_model_extension.rb
+++ lib/sortable/models/active_record_model_extension.rb
@@ -3,7 +3,7 @@ module Sortable
     extend ActiveSupport::Concern
 
     included do
-      def self.sort(sort, direction)
+      def self.sort(sort='created_at', direction='desc')
         order(sort + " " + direction)
       end
     end
