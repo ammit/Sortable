737723d7b3abb8b24f0872572e5c94a978a00748
Amit K <amit@lainfotech.in>
2017-09-20 02:48:12 +0530

    added model method

--- lib/sortable/helpers/action_view_extension.rb
+++ lib/sortable/helpers/action_view_extension.rb
@@ -2,8 +2,8 @@ module Sortable
   module ActionViewExtension
     def sortable(column, title = nil)
       title ||= column.titleize
-      css_class = column == sort_column ? "current #{sort_direction}" : nil
-      direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+      css_class = column == params[:sort] ? "current #{sort_direction}" : nil
+      direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
       link_to title, {:sort => column, :direction => direction}, {:class => css_class}
     end
   end
